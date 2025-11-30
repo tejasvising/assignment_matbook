a40c26782b8d2ba117cf8f4aec31d45993437c86
tejasvising <singhtejasvi.2001@gmail.com>
2025-11-30 19:29:47 +0530

    postcss fix

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -1,6 +1,7 @@
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 };
